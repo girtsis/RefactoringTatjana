fee64b1eb20f138dbfab09a0e7c8b7fa21eb8c5f
Daniel <girtsis.daniel@gmail.com>
2023-07-04 18:03:57 +0300

    Initial commit

---
+++ rentalPrice.js
@@ -0,0 +1,61 @@
+// age - age of driver
+// licence - number of full years person holds driving licence
+// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
+// acc - has s/he caused any accidents within last year
+// acc2 - has s/he participated (but not caused) in any accidents within last year
+// season - if it is high season or not
+
+function canRent(age, licence, rentalClass){
+  if (age < 18)
+  {
+    return "Driver too young - cannot quote the price";
+  }
+
+  if (age <= 21 && rentalClass >= 2)
+  {
+      return "Drivers 21 y/o or less can only rent Class 1 vehicles";
+  }
+
+  if (licence < 1)
+  {
+      return "Driver must hold driving licence at least for one year. Can not rent a car!";
+  }
+
+  return null;
+
+}
+
+function calculatePrice(rentalPrice, licence, age, highSeason, causedAccidents){
+
+  var rentalPrice = age;
+  if (rentalClass >= 4 && age <= 25 && highSeason === true)
+  {
+    rentalPrice *= 1.5;
+  }
+  if (licence < 3)
+  {
+    rentalPrice *= 1.3;
+  }
+  if (causedAccidents == true && age < 30)
+  {
+    rentalPrice += 15;
+  }
+  if (rentalPrice > 1000)
+  {
+    return 1000.0;
+  }
+  return rentalPrice;
+
+}
+
+function price(age, licence, rentalClass, causedAccidents, highSeason) {
+
+    const message = canRent(age, licence, rentalClass);
+    if(message){
+      return message
+    }
+
+    return calculatePrice(rentalPrice, licence, age, highSeason, causedAccidents);
+
+}
+exports.price = price;
